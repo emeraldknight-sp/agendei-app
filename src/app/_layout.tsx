7edf747c043aca3f8247b274d8ca85f3960a8cf0
David Almeida <contato.davidalmeida@outlook.com>
2024-10-21 21:07:13 -0300

    🚚 Move or rename resources

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,18 @@
-import { Tabs } from "expo-router";
+import { Stack } from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs
+    <Stack
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Tabs.Screen
+      <Stack.Screen
         name="index"
         options={{
           title: "Home",
         }}
       />
-    </Tabs>
+    </Stack>
   );
 }
---
+++ src/app/index.tsx
@@ -0,0 +1,5 @@
+import { Login } from "@/src/screens/login";
+
+export default function HomeScreen() {
+  return <Login />;
+}
