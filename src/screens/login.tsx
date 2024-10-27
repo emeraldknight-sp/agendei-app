7cb96d75cdbba9729919e56509518debc30fcc88
David Almeida <contato.davidalmeida@outlook.com>
2024-10-27 18:16:30 -0300

    ✅ Update: Login screen

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,45 +1,30 @@
 import { Button } from "../components/button";
+import { Image, Text, View } from "react-native";
 import { Input } from "../components/input";
 import { Link } from "expo-router";
-import {
-  Image,
-  SafeAreaView,
-  StatusBar,
-  Text,
-  View,
-} from "react-native";
+import { logo } from "../constants/icons";
 
 export const Login = () => {
   const signIn = () => console.log("ENTRANDO");
-  const currentStatusBarHeight = StatusBar.currentHeight;
 
   return (
-    <SafeAreaView
-      className="flex-1"
-      style={{ marginTop: currentStatusBarHeight }}
-    >
-      <View className="flex-1 justify-between bg-white px-4 py-4">
-        <View className="items-center">
-          <Image
-            source={require("@/src/assets/logo.png")}
-            className="w-24 h-5"
-            alt="logotipo"
-          />
-        </View>
-        <View style={{ gap: 20 }}>
-          <Input placeholder="Email" enterKeyHint="next" inputMode="email" />
-          <Input placeholder="Senha" enterKeyHint="next" isPasswordInput />
-          <Button text="Entrar" onPress={signIn} />
-        </View>
-        <View className="items-center">
-          <Text className="text-gray-6">
-            Não tenho conta.&nbsp;
-            <Link href="/" className="text-blue-500 font-medium underline">
-              Criar conta agora
-            </Link>
-          </Text>
-        </View>
+    <View className="flex-1 justify-between bg-white px-4 py-4">
+      <View className="items-center">
+        <Image source={logo} className="w-24 h-5" alt="logotipo" />
       </View>
-    </SafeAreaView>
+      <View style={{ gap: 20 }}>
+        <Input placeholder="Email" enterKeyHint="next" inputMode="email" />
+        <Input placeholder="Senha" enterKeyHint="next" isPasswordInput />
+        <Button text="Entrar" onPress={signIn} />
+      </View>
+      <View className="items-center">
+        <Text className="text-zinc-700">
+          Não tenho conta.&nbsp;
+          <Link href="/" className="text-blue-500 font-medium underline">
+            Criar conta agora
+          </Link>
+        </Text>
+      </View>
+    </View>
   );
 };
