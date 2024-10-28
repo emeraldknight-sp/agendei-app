cf5a1e7268c2e55d66b9d212f922e0c06511d929
David Almeida <contato.davidalmeida@outlook.com>
2024-10-28 11:53:11 -0300

    ✅ Update: Profile screen

--- src/screens/profile.tsx
+++ src/screens/profile.tsx
@@ -1,5 +1,21 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export const Profile = () => {
-  return <View />;
+  const infos = [
+    { title: "Nome", value: "David Almeida" },
+    { title: "Email", value: "davyd.aguiar@hotmail.com" },
+  ];
+
+  return (
+    <View className="bg-white flex-1">
+      {infos.map((info) => (
+        <View className="border-y-[1px] border-zinc-300 p-4">
+          <Text className="text-sm text-zinc-500 font-semibold">
+            {info.title}
+          </Text>
+          <Text className="text-lg text-zinc-700">{info.value}</Text>
+        </View>
+      ))}
+    </View>
+  );
 };
