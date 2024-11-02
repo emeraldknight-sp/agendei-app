8510ad36c1c842a6f368fbe7a64b51a2d39015c6
David Almeida <contato.davidalmeida@outlook.com>
2024-11-01 21:16:02 -0300

    ✅ Update: Layout and index files

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,21 +1,50 @@
+import { Feather } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Stack } from "expo-router";
+import colors from "tailwindcss/colors";
+import { logo } from "../constants/icons";
+import { Header } from "../components/header";
 
 export default function AppLayout() {
   return (
     <SafeAreaView className="flex-1">
-      <Stack
+      <Tabs
         screenOptions={{
-          headerShown: false,
+          tabBarActiveTintColor: colors.blue[500],
+          tabBarInactiveTintColor: colors.zinc[300],
+          tabBarShowLabel: false,
         }}
       >
-        <Stack.Screen
+        <Tabs.Screen
           name="index"
           options={{
-            title: "Home",
+            header: () => <Header />,
+            tabBarIcon: ({ color }) => (
+              <Feather name="home" size={24} color={color} />
+            ),
           }}
         />
-      </Stack>
+        <Tabs.Screen
+          name="calendar"
+          options={{
+            header: () => <Header text="Minhas reservas" />,
+            tabBarIcon: ({ color }) => (
+              <Feather name="calendar" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            header: () => <Header text="Meu perfil" />,
+            tabBarIcon: ({ color }) => (
+              <Feather name="user" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen name="auth" options={{ href: null }} />
+      </Tabs>
     </SafeAreaView>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,20 @@
-import { Calendar } from "../screens/calendar";
-import { Home } from "../screens/home";
-import { Login } from "../screens/login";
-import { Register } from "../screens/register";
+import { CardDoctor } from "../components/card-doctor";
+import { FlatList, Text, View } from "react-native";
+import { doctors } from "../constants/data";
 
 export default function App() {
-  return <Calendar />;
+  return (
+    <View className="bg-white justify-between flex-1 space-y-1 px-5">
+      <Text className="text-base text-zinc-700">
+        Agende os seus serviços médicos
+      </Text>
+      <FlatList
+        data={doctors}
+        keyExtractor={(item) => item.id_doctor.toString()}
+        renderItem={({ item }) => <CardDoctor data={item} />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingVertical: 8 }}
+      />
+    </View>
+  );
 }
