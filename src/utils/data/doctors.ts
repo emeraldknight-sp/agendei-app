fd7feb4906cbb01337bf12c433d7054392da5152
David Almeida <contato.davidalmeida@outlook.com>
2024-11-05 21:40:17 -0300

    🤡 Mock things

---
+++ src/utils/data/doctors.ts
@@ -0,0 +1,56 @@
+export const doctors = [
+  {
+    id_doctor: 1,
+    name: "Dr. Armando Matheus",
+    specialty: "Ginecologia e obstetrícia",
+    icon: "M",
+  },
+  {
+    id_doctor: 2,
+    name: "Dra. Ana Beatriz Rutini",
+    specialty: "Cardiologista",
+    icon: "F",
+  },
+  {
+    id_doctor: 3,
+    name: "Dr. Antônio Almeida Souza ",
+    specialty: "Pediatria",
+    icon: "M",
+  },
+  {
+    id_doctor: 4,
+    name: "Dra. Roberta Martins",
+    specialty: "Clínica Geral",
+    icon: "F",
+  },
+  {
+    id_doctor: 5,
+    name: "Dra. Nise da Silveira",
+    specialty: "Cirurgia Plástica",
+    icon: "F",
+  },
+  {
+    id_doctor: 6,
+    name: "Dr. Jonatan Silvestre",
+    specialty: "Pediatria",
+    icon: "M",
+  },
+  {
+    id_doctor: 7,
+    name: "Dr. José Eduardo Souza",
+    specialty: "Clínica Geral",
+    icon: "M",
+  },
+  {
+    id_doctor: 8,
+    name: "Dra. Adriana Melo",
+    specialty: "Clínica Geral",
+    icon: "F",
+  },
+  {
+    id_doctor: 9,
+    name: "Dra. Valeria Petri",
+    specialty: "Cirurgia Plástica",
+    icon: "F",
+  },
+];
---
+++ src/utils/data/doctors_services.ts
@@ -0,0 +1,22 @@
+export const doctors_services = [
+  {
+    id_service: 1,
+    description: "Consulta Médica",
+    price: 499.99,
+  },
+  {
+    id_service: 2,
+    description: "Drenagem Linfática",
+    price: 650,
+  },
+  {
+    id_service: 3,
+    description: "Lipoaspiração",
+    price: 5000,
+  },
+  {
+    id_service: 4,
+    description: "Mamoplastia",
+    price: 1700,
+  },
+];
