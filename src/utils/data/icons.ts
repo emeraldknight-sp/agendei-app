3e5ec19807b34c327cb3d3163e40749334aa7ec2
David Almeida <contato.davidalmeida@outlook.com>
2024-11-05 21:54:58 -0300

    ✅ Update: Sorting alphabetically icons imports

--- src/utils/data/icons.ts
+++ src/utils/data/icons.ts
@@ -1,10 +1,10 @@
-import home from "../assets/home.png";
-import calendar from "../assets/calendar.png";
-import profile from "../assets/profile.png";
-import logo from "../assets/logo.png";
-import male from "../assets/male.png";
-import female from "../assets/female.png";
-import clock from "../assets/clock.png";
-import biglogo from "../assets/biglogo.png";
+import biglogo from "../../assets/biglogo.png";
+import calendar from "../../assets/calendar.png";
+import clock from "../../assets/clock.png";
+import female from "../../assets/female.png";
+import home from "../../assets/home.png";
+import logo from "../../assets/logo.png";
+import male from "../../assets/male.png";
+import profile from "../../assets/profile.png";
 
 export { home, calendar, profile, logo, male, female, clock, biglogo };
